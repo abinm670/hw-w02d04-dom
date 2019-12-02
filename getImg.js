69b7bb2824bbc5b874e9600ee4c09e80ca7f6b7c
Abdullah Binmahfouz <abinm670@mtroyal.ca>
2019-12-03 01:42:36 +0300

    add image resizing function work

--- getImg.js
+++ getImg.js
@@ -1,29 +1,37 @@
-window.onload = function() {
-  // /* get the element*/
+// window.onload = function () {
+// /* get the element*/
 
-  var newImg = document.createElement("img");
-  var src = document.createAttribute("src");
-  var id = document.createAttribute("id");
-  var width = document.createAttribute("width");
-  var hight = document.createAttribute("hight");
+var newImg = document.createElement("img");
+var src = document.createAttribute("src");
+var id = document.createAttribute("id");
+var width = document.createAttribute("width");
+var hight = document.createAttribute("hight");
 
-  document.body.appendChild(newImg);
+//select address and create a dom for this address
+var img = document.querySelector(".image1")
+//place the new element in the node address
+img.appendChild(newImg);
 
-  //set Attributes to the new img node
+//set Attributes to the new img node
+ewImg.setAttribute("id", "changeSize");
 
-  newImg.setAttribute("id", "changeSize");
 
-  //get the value from the input
-  var width1 = input.getAttribute("width");
-  var hight1 = input.getAttribute("hight");
 
-  var myButton = document.querySelector("#randomize");
 
-  myButton.addEventListener("click", function() {
-    //node element add new src with new src atribu that been created.
-    document.getElementById("changeSize").width = width1;
-    document.getElementById("changeSize").hight = hight1;
-    newImg.setAttribute("src", "https://source.unsplash.com/1600x900");
-  });
-  // };
-};
+//sect the area for the button
+var myButton = document.querySelector("#randomize");
+
+//create add listener
+myButton.addEventListener("click", function () {
+  //select the the location and create a dom for this location
+  var hight = document.querySelector(".height")
+  var width = document.querySelector(".width")
+
+  //assign a vlaue to this location to the value that we get form the input
+  document.querySelector("#changeSize").width = width.value;
+  document.querySelector("#changeSize").height = hight.value;
+  newImg.setAttribute("src", "https://source.unsplash.com/1600x900");
+
+});
+
+  //happyCoding 
